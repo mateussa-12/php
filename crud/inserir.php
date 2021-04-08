5e34c04e201c97331be2b342e8c453bea37e1b49
Mateus Santos <mateus.sa94@hotmail.com>
2021-04-08 11:01:36 -0300

    criei o arquivo inserir.php

---
+++ crud/inserir.php
@@ -0,0 +1,17 @@
+<?php
+	require("conexao.php");
+	
+	$nome = $_POST['txNome'];
+	$email = $_POST['txEmail'];
+	$senha = $_POST['txSenha'];
+	
+	try{
+		$stmt = $conn->prepare("insert into tb1 values(null,'$nome','$email','$senha');");
+		$stmt ->execute();
+		
+		header("location:cadastro.php")
+		}
+		catch(PDOException $e){
+			echo "Erro: " . $e -> getMessage();
+		}
+?>
